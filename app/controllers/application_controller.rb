fe23108b229457457a17008abd90fe612af22a67
Cristian Ogliari <cogliari@gmail.com>
2020-03-07 14:16:04 -0300

    correction for user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,7 @@ class ApplicationController < ActionController::Base
   end
   
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit :sign_up, keys: %i[name, username]
+    devise_parameter_sanitizer.permit :sign_up, keys: %i[name username]
   end
   
-
 end
